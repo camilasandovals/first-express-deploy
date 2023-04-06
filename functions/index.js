ced2e23f1b874647ef3b24ecdbb003224fd38e3a
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-06 13:16:43 -0400

    working API deployed

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send("My cloud function API is working! 🥹");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello there");
+});
+
+export const api = functions.https.onRequest(app);
